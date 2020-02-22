6a1c380dc528d50adbddb9110a65ad0fc1f3eb64
nerdjpg <60404582+nerdjpg@users.noreply.github.com>
2020-02-22 07:32:43 -0500

    Add files via upload

---
+++ projects/schoolprojects/386/405/data/forweb_2.js
@@ -0,0 +1,27 @@
+var json_forweb_2 = {
+"type": "FeatureCollection",
+"name": "forweb_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "Name": "CERA Pond", "I1957": "YES", "X": "-76.71321297", "Y": "39.2506269300001", "WP": "PP01" }, "geometry": { "type": "Point", "coordinates": [ -76.713212969999972, 39.250626930000074 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "Name": "Houses Near Campus", "I1957": "YES", "X": "-76.717415134", "Y": "39.2539597540001", "WP": "PP08" }, "geometry": { "type": "Point", "coordinates": [ -76.717415133999964, 39.25395975400005 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "3", "Name": "Knoll", "I1957": "YES", "X": "-76.7102858819999", "Y": "39.2523101150001", "WP": "PP04" }, "geometry": { "type": "Point", "coordinates": [ -76.710285881999937, 39.25231011500005 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4", "Name": "Alumni House", "I1957": "YES", "X": "-76.716768993", "Y": "39.258268753", "WP": "PP13" }, "geometry": { "type": "Point", "coordinates": [ -76.71676899299996, 39.258268753000038 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "5", "Name": "Hilltop Circle", "I1957": "NO", "X": "-76.7196530269999", "Y": "39.257326671", "WP": "PP12" }, "geometry": { "type": "Point", "coordinates": [ -76.719653026999936, 39.257326671000051 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "6", "Name": "Library", "I1957": "NO", "X": "-76.711295487", "Y": "39.2566297380001", "WP": "PP19" }, "geometry": { "type": "Point", "coordinates": [ -76.711295486999973, 39.256629738000072 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "7", "Name": "Sondiem", "I1957": "NO", "X": "-76.712839932", "Y": "39.253465916", "WP": "PP06" }, "geometry": { "type": "Point", "coordinates": [ -76.712839931999952, 39.253465916000039 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "8", "Name": "Lot 8", "I1957": "NO", "X": "-76.71497126", "Y": "39.255994894", "WP": "PP11" }, "geometry": { "type": "Point", "coordinates": [ -76.71497126, 39.255994894000018 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "9", "Name": "Baseball Field", "I1957": "NO", "X": "-76.708399992", "Y": "39.2499776430001", "WP": "PP03" }, "geometry": { "type": "Point", "coordinates": [ -76.708399991999954, 39.249977643000079 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "10", "Name": "Potomic Hall", "I1957": "NO", "X": "-76.706827381", "Y": "39.255931957", "WP": "PP17" }, "geometry": { "type": "Point", "coordinates": [ -76.706827380999982, 39.25593195700003 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "11", "Name": "BC Court House", "I1957": "NO", "X": "-76.71536794", "Y": "39.260116602", "WP": "PP15" }, "geometry": { "type": "Point", "coordinates": [ -76.715367939999965, 39.260116602000039 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "12", "Name": "Eirkson Field", "I1957": "YES", "X": "-76.710120734", "Y": "39.2561887750001", "WP": "PP20" }, "geometry": { "type": "Point", "coordinates": [ -76.710120733999986, 39.256188775000062 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "13", "Name": "Fine Arts Building", "I1957": "NO", "X": "-76.713832289", "Y": "39.255485768", "WP": "PP10" }, "geometry": { "type": "Point", "coordinates": [ -76.713832288999981, 39.255485768000028 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "14", "Name": "Tennis Courts", "I1957": "NO", "X": "-76.7118397519999", "Y": "39.252819737", "WP": "PP05" }, "geometry": { "type": "Point", "coordinates": [ -76.711839751999946, 39.252819737000038 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "15", "Name": "Trees Near Campus", "I1957": "YES", "X": "-76.716857115", "Y": "39.2555028880001", "WP": "PP09" }, "geometry": { "type": "Point", "coordinates": [ -76.716857114999982, 39.25550288800008 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "16", "Name": "Naval ROTC", "I1957": "NO", "X": "-76.7172418069999", "Y": "39.2584821610001", "WP": "PP14" }, "geometry": { "type": "Point", "coordinates": [ -76.717241806999937, 39.258482161000082 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "17", "Name": "CERA Stream", "I1957": "YES", "X": "-76.7113421645046", "Y": "39.2504360790852", "WP": "PP02" }, "geometry": { "type": "Point", "coordinates": [ -76.7113421645046, 39.250436079085169 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "18", "Name": "Forest Patch", "I1957": "YES", "X": "-76.7051233916147", "Y": "39.2570946424615", "WP": "PP18" }, "geometry": { "type": "Point", "coordinates": [ -76.705123391614663, 39.257094642461531 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "19", "Name": "Forest Patch (Narnia)", "I1957": "YES", "X": "-76.7103998251685", "Y": "39.2592125188805", "WP": "PP16" }, "geometry": { "type": "Point", "coordinates": [ -76.710399825168537, 39.259212518880474 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "20", "Name": "Forest Patch near housing", "I1957": "YES", "X": "-76.7167075193708", "Y": "39.2533707636946", "WP": "PP07" }, "geometry": { "type": "Point", "coordinates": [ -76.716707519370814, 39.25337076369464 ] } }
+]
+}
